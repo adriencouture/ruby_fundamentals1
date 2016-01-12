87db90e725f4c39b1a1930ae697cdad12db4be0e
Adrien Couture <coutureadrien@gmail.com>
2016-01-12 14:43:08 -0500

    Exercises 1,2 & 3 complete

--- exercise3.rb
+++ exercise3.rb
@@ -4,8 +4,23 @@
 # puts "What's your name?"
 # my_name = gets
 # puts my_name
+#
+# puts "What's your name?"
+# my_name = gets.chomp
+#
+# puts "Hi #{my_name}!"
+#
+# puts "What's your name?"
+# my_name = gets.strip # Gets rid of spaces
+#
+# puts "Hi #{my_name}!"
+#
+# puts "How old are you?"
+# age = gets.chomp
+# puts "You were born in #{2016 - age.to_i}"
 
-puts "What's your name?"
-my_name = gets.chomp
+# OR
 
-puts "Hi #{my_name}!"
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "You were born in #{2016 - age}"
