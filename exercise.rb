f6e0bd175961110f4b71a14cf4c0bd4509703818
Adrien Couture <coutureadrien@gmail.com>
2016-01-12 13:57:48 -0500

    Exercise 1 & 2 updates

--- exercise.rb
+++ exercise.rb
@@ -7,3 +7,6 @@ puts 3
 puts 2 + 3
 
 # puts 2 puts 3 puts 2 + 3
+
+
+puts "1. Hello\n2. world"
---
+++ exercise2.rb
@@ -0,0 +1,28 @@
+=begin Question 1 - Find meal cost plus "decent" tip,
+then print the tip amount by subtracting the bill cost before the tip!
+=end
+
+meal_cost = 55 * 1.18
+  puts meal_cost - 55
+
+# OR
+
+# puts (55 * 1.18) - 55
+  puts 55 * 0.18
+
+# Question 2 - Convert the Integer to print result
+
+puts 4.to_s + " String"
+
+# Question 3 - Turn numbers into variables, then print the multiplication
+
+number1 = 45628
+number2 = 7839
+
+puts (number1 * number2)
+
+=begin Question 4 - I just wrote the expression, printed it to the screen.
+The value is True.
+=end
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
